c4997af41d9143b34fcc68e86afa20e4d3fac850
alejandro <alejandro-donoso@hotmail.com>
2021-11-05 19:00:43 -0300

    added comments, and a file to test with.

--- add_qr_to_png.py
+++ add_qr_to_png.py
@@ -5,26 +5,34 @@
 
 # In[1]:
 
-#WE NEED 2 PNGS
+!pip install pyqrcode 
+!pip install pypng
+!pip install Pillow
+
+# in[2]
 import pyqrcode
 from PIL import Image
-
-img_bg = Image.open('BACKGROUND.png')
-
+#Open background
+img_bg = Image.open('background.png')
+#Create QR
 url = pyqrcode.QRCode('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-url.png('FRESH-QR.png', scale = 2)
-
-img_qr = Image.open('FRESH-QR.png')
-
-pos = (img_bg.size[0] - img_qr.size[0], img_bg.size[1] - img_qr.size[1])
-
+#Save QR as png
+url.png('New_QR.png', scale = 2)
+#Open QR as a variable
+img_qr = Image.open('New_QR.png')
+#if we want to touch the corner this should be 0 then.
+pixels_away_from_corner = 10
+#Calculate position to place the qr acording to qr size and background size.
+pos = (img_bg.size[0] - img_qr.size[0] - pixels_away_from_corner, img_bg.size[1] - img_qr.size[1] - pixels_away_from_corner)
+#We create the png
 img_bg.paste(img_qr, pos)
-img_bg.save('export-2.png')
+#We save the png
+img_bg.save('export.png')
 
 
-# In[2]:
-
+# In[3]:
 
 from IPython.display import Image
-Image(filename='export-2.png') 
+#Open our new PNG
+Image(filename='export.png') 
 
